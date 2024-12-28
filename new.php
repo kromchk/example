e08610148955e53fab1f6f06629a4ae2e1b402e0
kromchk <kromchk@proton.me>
2024-12-28 20:45:49 +0200

    upload new.php

--- new.php
+++ new.php
@@ -1,3 +1,22 @@
 <?php
-class Singleton {
+class Singlenton {
+	private static $instance = null;
+	private function __construct () {
+		echo "Instance of class created";
+	}
+	public getInstance() {
+		if (self::$instance === null) {
+			self::$instance = new static();
+		}
+		return self::$instance;
+	}
+	private function clone() {}
+	private function wakeup() {}
+	public function doSomething() {
+		echo "Class exist";
+	}		
 }
+
+
+$single = Singlenton();
+echo "Value of class instance " . $single::getInstance();
