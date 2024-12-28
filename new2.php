7dad01ede0b987d7fb78f8e6d84635ef985ca1ad
kromchk <kromchk@proton.me>
2024-12-28 20:46:30 +0200

    upload new2.php

--- new2.php
+++ new2.php
@@ -1,1 +1,21 @@
 <?php
+
+interface Tag {
+	public function setPosition($numpage);
+}
+
+public class Book implements Tag {
+	private int $numpage;
+	
+	public function setPosition($numpage) {
+		$this->numpage = $numpage;
+	}
+}
+
+public class Notepad implements Tag {
+	private int $numpage;
+	
+	public function setPosition($numpage) {
+		$this->numpage = $numpage;
+	}
+}
